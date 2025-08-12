e690ed1429dac1010c63d6b0698007203c63960d
Uttam <uttam.pachikayala@students.mq.edu.au>
2025-08-12 18:43:22 +1000

    Finished WK2 Task 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Red vs. Blue");
+        System.out.println("~~~~~");
+        System.out.println("COMP2000 2025");
+        System.out.println("~~~~~");
     }
 }
